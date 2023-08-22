8e93c248414ac953f55374a9faaf3f25c83b6c6f
Your Name <sinigami.ninja@gmail.com>
2023-08-22 15:39:20 +0700

    cors3

--- public/js/script.js
+++ public/js/script.js
@@ -149,7 +149,7 @@ const sendReq = async function (data) {
   try {
     const reqGet = await fetch(`https://xn--24-ilci9b.xn--p1ai:4337`, {
       method: "POST", // *GET, POST, PUT, DELETE, etc.
-      mode: "no-cors", // no-cors, *cors, same-origin
+      mode: "cors", // no-cors, *cors, same-origin
       cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
       credentials: "same-origin", // include, *same-origin, omit
       headers: {
