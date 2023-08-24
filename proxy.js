b675adca5c5c264a605d645206421418f4ae925d
Your Name <sinigami.ninja@gmail.com>
2023-08-24 19:57:49 +0700

    res.end

--- proxy.js
+++ proxy.js
@@ -114,6 +114,8 @@ app.post("/message", (req, res) => {
       text: `https://yandex.ru/maps/?ll=${data.lng}%2C${data.lat}&mode=whatshere&whatshere%5Bpoint%5D=${data.lng}%2C${data.lat}&whatshere%5Bzoom%5D=16&z=16`,
     }),
   });
+
+  res.end();
 });
 
 app.listen(PORT, () => {
